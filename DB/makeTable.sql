24e4d4b7777e51b0c286116e6a0d19f08df3eafa
川原大宙 <k_hiro@kzd.biglobe.ne.jp>
2022-06-05 17:14:55 +0900

    tablesの作成

---
+++ DB/makeTable.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS cards;
+DROP TABLE IF EXISTS tags;
+
+CREATE TABLE users (
+user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+customer_name VARCHAR(100) NOT NULL,
+user_level INT NOT NULL,
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL,
+PRIMARY KEY(user_id)
+);
+
+CREATE TABLE tags (
+tag_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+created_user_id INT UNSIGNED NOT NULL,
+tag_name VARCHAR(100) NOT NULL,
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL,
+PRIMARY KEY(tag_id),
+FOREIGN KEY (created_user_id) REFERENCES users(user_id)
+);
+
+
+CREATE TABLE cards (
+card_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+tag_id INT UNSIGNED NOT NULL,
+lerning_level INT NOT NULL,
+question_text VARCHAR(200) NOT NULL,
+answer_text VARCHAR(200) NOT NULL,
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL,
+PRIMARY KEY(card_id),
+FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
+);
