569e09a6f2043c8eb5dafa00e597727f22cb93ec
川原大宙 <k_hiro@kzd.biglobe.ne.jp>
2022-06-30 12:47:05 +0900

    ハンドラのテストを追加途中

--- testCardHanler.go
+++ testCardHanler.go
@@ -1,8 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http/httptest"
+	"testing"
+)
 
-import "testing"
+func TestHandleCardReuest(t *testing.T) {
 
+	r := httptest.NewRequest("GET", "/card/1", nil)
+	w := httptest.NewRecorder()
 
+	handleCardRequest(w, r)
 
-func TestHandleCard
+	resp := w.Result()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("cannot read test response: %v", err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Errorf("got = %d, want = 200", resp.StatusCode)
+	}
+
+	fmt.Println(body)
+	// if string(body) != "hello world!" {
+	// 	t.Errorf("got = %s, want = hello world!", body)
+	// }
+
+}
