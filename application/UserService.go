0d25865df39e1839445ce2e0c3fd88bfe400f3e9
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-08-04 18:56:35 +0900

    userServiceの作成途中

---
+++ application/UserService.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/sakana7392/AnkiCard_server/domain/model"
+)
+
+//	emaliとpasswordが存在するかチェック
+func checkEmailAndPassword(email, password string) bool {
+
+	var User model.User
+
+	fmt.Println(User)
+
+	return true
+}
+
+//	新規作成時にemailとパスワードをチェック
+func createNewUser() {
+
+}
