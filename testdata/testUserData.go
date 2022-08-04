26294774cadd6a11656b46afb4c891f388a4853c
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-08-04 20:25:07 +0900

    ユーザのサンプルデータを追加

---
+++ testdata/testUserData.go
@@ -0,0 +1,25 @@
+import "github.com/sakana7392/AnkiCard_server/domain/model"
+
+var TestUserData User[]=[]User{
+    {
+        userId: 1,
+        userName: "sakana",
+        NextQuestionId,3,
+        userLevel: 1,
+        password: "password",
+    },
+    {
+        userId: 2,
+        userName: "sakana2",
+        NextQuestionId,5,
+        userLevel: 2,
+        password: "password2",
+    },
+    {
+        userId: 3,
+        userName: "sakana3",
+        NextQuestionId,4,
+        userLevel: 1,
+        password: "password3",
+    },
+}
