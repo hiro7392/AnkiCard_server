0f630561700e4a3edc475b745fa988b67c109da3
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-08-24 00:15:42 +0900

    サンプルデータを修正

--- testdata/testTagData.go
+++ testdata/testTagData.go
@@ -4,19 +4,51 @@ import "github.com/sakana7392/AnkiCard_server/domain/model"
 
 var TestTagData []model.Tag = []model.Tag{
 	{
-		CreatedUserId: 2,
+		CreatedUserId: 1,
 		TagName:       "TCP/IP",
 	},
 	{
-		CreatedUserId: 3,
+		CreatedUserId: 1,
 		TagName:       "データベース",
 	},
 	{
-		CreatedUserId: 4,
+		CreatedUserId: 1,
 		TagName:       "英語",
 	},
 	{
-		CreatedUserId: 2,
+		CreatedUserId: 1,
 		TagName:       "アルゴリズム",
+	},
+	{
+		CreatedUserId: 2,
+		TagName:       "TCP/IP 2",
+	},
+	{
+		CreatedUserId: 2,
+		TagName:       "データベース 2",
+	},
+	{
+		CreatedUserId: 2,
+		TagName:       "英語 2",
+	},
+	{
+		CreatedUserId: 2,
+		TagName:       "アルゴリズム 2",
+	},
+	{
+		CreatedUserId: 3,
+		TagName:       "TCP/IP 3",
+	},
+	{
+		CreatedUserId: 3,
+		TagName:       "データベース 3",
+	},
+	{
+		CreatedUserId: 3,
+		TagName:       "英語 3",
+	},
+	{
+		CreatedUserId: 3,
+		TagName:       "アルゴリズム 3",
 	},
 }
