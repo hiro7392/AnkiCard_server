a107e0f9fc45c5302247052015f504777e84176f
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2022-10-05 20:09:32 +0900

    環境変数を利用するように変更

--- infra/databaseMangage.go
+++ infra/databaseMangage.go
@@ -3,7 +3,10 @@ package infra
 import (
 	"database/sql"
 	"log"
+	"os"
+	"time"
 
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql" //コード内で直接参照するわけではないが、依存関係のあるパッケージには最初にアンダースコア_をつける
 )
 
@@ -18,13 +21,25 @@ type NullableCard struct {
 	QuestionText  string
 	AnswerText    string
 }
+
 func init() {
 	var err error
-	Db, err = sql.Open("mysql", "root:mysql@/AnkiCard?parseTime=true")
+	//Db, err = sql.Open("mysql", "root:mysql@/AnkiCard?parseTime=true")
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	c := mysql.Config{
+		DBName:    os.Getenv("DB_NAME"),
+		User:      os.Getenv("DB_USER"),
+		Passwd:    os.Getenv("DB_PASSWORD"),
+		Addr:      os.Getenv("DB_ADDR"),
+		Net:       "tcp",
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+	Db, err = sql.Open("mysql", c.FormatDSN())
 
 	if err != nil {
 		panic(err.Error())
 	}
-
 	log.Println("init success!\n Connected to mysql")
 }
